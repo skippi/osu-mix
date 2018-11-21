6e656f843514d18ed854a26f0a900fd06ebb5dde
Justin <jbtcao@gmail.com>
2018-11-20 21:48:07 -0600

    Added effect volume option

--- osumix/shell.py
+++ osumix/shell.py
@@ -10,12 +10,13 @@ from .track import Track
 
 
 @click.command(options_metavar='[options]')
-@click.argument('input', nargs=1, metavar='<input>')
-@click.argument('output', nargs=1, metavar='<output>')
 @click.option('--beatmap-sounds', nargs=1, help='beatmap sounds directory', metavar='<dir>')
+@click.option('--effect-volume', nargs=1, type=float, default=1.0, help='effect volume (default: 1.0)', metavar='<float>')
 @click.option('--music', nargs=1, help='music audio', metavar='<file>')
 @click.option('--music-volume', nargs=1, type=float, default=1.0, help='music volume (default: 1.0)', metavar='<float>')
-def main(input, output, beatmap_sounds, music, music_volume):
+@click.argument('input', nargs=1, metavar='<input>')
+@click.argument('output', nargs=1, metavar='<output>')
+def main(beatmap_sounds, effect_volume, music, music_volume, input, output):
     """Compiles a beatmap <input> to an audio file <output>."""
     output_format = os.path.splitext(output)[1][1:]
 
@@ -24,6 +25,7 @@ def main(input, output, beatmap_sounds, music, music_volume):
     beatmap = Beatmap.from_path(input)
     track = Track.from_beatmap(beatmap, bm_audios)
     beatmap_audio = track.compile()
+    beatmap_audio = audioseg_adjust_volume(beatmap_audio, effect_volume)
 
     result = beatmap_audio
 
