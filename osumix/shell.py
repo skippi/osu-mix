ff9485dd30be44e1fc1aa08b3fed811cbc4440c3
Justin <jbtcao@gmail.com>
2018-11-20 20:09:16 -0600

    Added CLI `beatmap-sounds` option
    Now we can use custom beatmap sounds with a specified CLI option.

--- osumix/shell.py
+++ osumix/shell.py
@@ -4,6 +4,7 @@ import sys
 import click
 from pydub import AudioSegment
 from slider.beatmap import Beatmap
+from .audio import load_sounds
 from .track import Track
 
 
@@ -11,12 +12,15 @@ from .track import Track
 @click.argument('input', nargs=1, metavar='<input>')
 @click.argument('output', nargs=1, metavar='<output>')
 @click.option('--audio', nargs=1, help='music audio', metavar='<file>')
-def main(input, output, audio):
+@click.option('--beatmap-sounds', nargs=1, help='beatmap sounds directory', metavar='<file>')
+def main(input, output, audio, bm_sounds):
     """Compiles a beatmap <input> to an audio file <output>."""
     output_format = os.path.splitext(output)[1][1:]
 
+    beatmap_sounds = load_sounds(bm_sounds) if bm_sounds else {}
+
     beatmap = Beatmap.from_path(input)
-    track = Track.from_beatmap(beatmap)
+    track = Track.from_beatmap(beatmap, beatmap_sounds)
     beatmap_audio = track.compile()
 
     result = beatmap_audio
